55b239eb4c92b1b8ecacbb3f76153e33f44112bf
Filip Ilioski <if39521@zut.edu.pl>
2018-11-20 17:42:16 +0100

    fixed bugs

--- ajax/passwordChange.php
+++ ajax/passwordChange.php
@@ -4,18 +4,18 @@ require_once(__DIR__.'/../controller/UserController.php');
 require_once(__DIR__.'/../classes/DB.php');
 
 if (!empty($_POST)) {
-    $user = json_decode($_COOKIE['logged_user']);
-    $user_role = $user->user_role;
-
+    if (!empty($_COOKIE['logged_user'])) {
+        $user = json_decode($_COOKIE['logged_user']);
+        $user_role = $user->user_role;
+    } else {
+        $user_role = false;
+    }
 	$user_controller = new UsersController($pdo);
 	$username = $_POST['selected_student'];
     $new_password = $_POST['new_password'];
     $confirm_new_password = $_POST['new_password_confirm'];
-	if ($user_controller->changeUserPassword($user_role, $username, $new_password, $confirm_new_password)) {
-		header("Location: ../welcome.php");
-		exit();
-	} else {
-		header("Location: ../forgotPassword.php");
-	}
+	$user_controller->changeUserPassword($user_role, $username, $new_password, $confirm_new_password);
+    header("Location: ../forgotPassword.php");
+    exit();
 }
 ?>
